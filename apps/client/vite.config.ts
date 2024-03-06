d66393c7a5c719afdc26966f99b69e0688d6fd31
juanse <juan.posso@epn.edu.ec>
2024-03-06 01:14:30 -0500

    Remove unused test file and update Vite configuration

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,4 +1,4 @@
-/// <reference types='vitest' />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
@@ -32,18 +32,4 @@ export default defineConfig({
     },
   },
 
-  test: {
-    globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/client',
-      provider: 'v8',
-    },
-  },
 });
