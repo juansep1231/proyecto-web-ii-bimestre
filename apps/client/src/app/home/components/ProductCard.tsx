819b51b451975197287ed507863e40adb694051a
Valery Vallejo <valery.antonella19@gmail.com>
2024-03-08 01:19:33 -0500

    feat: create home page and components

---
+++ apps/client/src/app/home/components/ProductCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface Product {
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+}
+
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  return (
+    <div className="border rounded-lg overflow-hidden shadow-lg w-80 px-5 py-8 flex flex-col items-center justify-between gap-3 h-[480px]">
+      <img className="h-72" src={product.image} alt={product.name} />
+      <div className="flex flex-col items-center text-center">
+        <h2 className="text-lg font-semibold">{product.name}</h2>
+        <p className="text-gray-600">{product.description}</p>
+        <div className="mt-4">
+          <p className="text-gray-800 text-lg font-semibold">
+            Precio: ${product.price}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ apps/client/src/app/home/homePage.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import ProductCard from './components/ProductCard';
+
+const HomePage: React.FC = () => {
+  //const [products, setProducts] = useState<any[]>([]);
+
+  /*useEffect(() => {
+    // Llama a la función fetchProducts para obtener los datos de los productos
+    async function fetchData() {
+      const data = await fetchProducts();
+      setProducts(data);
+    }
+    fetchData();
+  }, []);*/
+
+  // Arreglo de productos
+  const products = [
+    {
+      id: 1,
+      name: 'Vestido',
+      image: '/img/vestido.png',
+      description: 'Vestido floreado casual de manga corta',
+      price: 35.0,
+    },
+    {
+      id: 2,
+      name: 'Producto 2',
+      image: '/img/vestido.png',
+      description: 'Vestido floreado casual de manga corta',
+      price: 20.99,
+    },
+    // Más productos...
+  ];
+
+  return (
+    <div className="p-12 flex flex-col items-center gap-12">
+      <h1 className="font-bold text-4xl">FASHION FUSION</h1>
+      <div className="flex justify-center gap-12">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
